e02886a1da0494aaea2c6401b5fa62fea85fcb5a
Sador <Zerezghi@live.co.uk>
2023-10-18 22:07:49 +0100

    Initialised Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 module.exports = {
-  purge: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'grayish-blue': 'hsl(217, 19%, 38%)',
+        'dark-blue': 'hsl(218, 23%, 16%)',
+        'box': 'hsl(217, 19%, 24%)',
+      },
+      width: {
+        'custom': '40rem',
+      },
+      height: {
+        'custom': '23rem', // You can adjust this value as needed
+      },
+    },
   },
   variants: {},
   plugins: [],
-}
+};
